e245c064ac97d8dba947691c290ad859dcdb2e3b
Leo Soubeste <leosoubeste@yahoo.com.ar>
2022-10-16 23:34:21 -0300

    Add model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ApplicationRecord
+    has_many :posts
 
+    validates :username, presence: true
+    validates :email, presence: true
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+
+    def blurred_email
+        start = email.index('@') + 1
+        the_end = email.rindex('.') - 1
+
+        domain = email[start..the_end]
+
+        email.gsub(domain, '*' * domain.length)
+    end
 end
