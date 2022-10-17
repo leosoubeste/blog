81ccf455b59804d5ed5cc3f490d36e57a0314dec
Leo Soubeste <leosoubeste@yahoo.com.ar>
2022-10-16 23:33:47 -0300

    Add tests for update controllers

--- spec/system/posts/update_spec.rb
+++ spec/system/posts/update_spec.rb
@@ -9,13 +9,48 @@ RSpec.describe 'update a post', type: :system do
     @post = Post.create(:user => @user1, :title => "My Blog Post", :body => "Hello world!\n\nThis is my first blog post.")
   end
 
-  scenario 'edit author' do
   ###TODO: complete this scenario 100% controller coverage
   ###Ability to change the author from Andrew to Matt
 
+  scenario 'empty title and body' do
+    visit edit_post_path(@post)
+
+    fill_in "Title", with: ""
+    fill_in "Body", with: ""
+
+    click_button 'Update Post'
+
+    expect(page).to have_content("Title can't be blank")
+    expect(page).to have_content("Body can't be blank")
+
+    expect(@post.reload.title).to eq("My Blog Post")
+    expect(@post.reload.body).to eq("Hello world!\n\nThis is my first blog post.")
   end
 
-  ###TODO: complete this spec 100% controller coverage
+  scenario 'edit author' do
+    visit edit_post_path(@post)
+    select @user2.username, from: "post[user_id]"
 
+    click_button 'Update Post'
+
+    expect(page).to have_content("Post was successfully updated.")
+
+    expect(@post.reload.user).to eq(@user2)
+  end
+
+  scenario 'edit title and body' do
+    visit edit_post_path(@post)
+    fill_in "Title", with: "Updated title"
+    fill_in "Body", with: "A new body for my post"
+
+    click_button 'Update Post'
+
+    expect(page).to have_content("Updated title")
+    expect(page).to have_content("By: Andrew")
+    expect(page).to have_content("A new body for my post")
+
+    expect(@post.reload.title).to eq("Updated title")
+    expect(@post.reload.body).to eq("A new body for my post")
+  end
   
 end
--- spec/system/users/update_spec.rb
+++ spec/system/users/update_spec.rb
@@ -3,6 +3,48 @@ require 'rails_helper'
 
 RSpec.describe 'update a user', type: :system do
   ###TODO: complete this spec 100% controller coverage
+  before(:each) do
+    @user1 = User.create(:username => "Andrew", :email => "achalkley@example.com") 
+    @user2 = User.create(:username => "Leo", :email => "leosoubeste@example.com") 
+  end
 
-  
+  scenario 'empty username and email' do
+    visit edit_user_path(@user1)
+
+    fill_in "Username", with: ""
+    fill_in "Email", with: ""
+
+    click_button 'Update User'
+
+    expect(page).to have_content("Username can't be blank")
+    expect(page).to have_content("Email can't be blank")
+
+    expect(@user1.reload.username).to eq("Andrew")
+    expect(@user1.reload.email).to eq("achalkley@example.com")
+  end
+
+  scenario 'invalid email' do
+    visit edit_user_path(@user1)
+    fill_in "Email", with: "invalid@email"
+    click_button 'Update User'
+
+    expect(page).to have_content("Email is invalid")
+
+    expect(@user1.reload.username).to eq("Andrew")
+    expect(@user1.reload.email).to eq("achalkley@example.com")
+  end
+
+  scenario 'valid user details' do
+    visit edit_user_path(@user2)
+    fill_in "Username", with: "Leo Soubeste"
+    fill_in "Email", with: "leosoubeste@other.com"
+    click_button 'Update User'
+
+    expect(page).to have_content("User was successfully updated.")
+    expect(page).to have_content("Leo Soubeste")
+    expect(page).to have_content("leosoubeste@*****.com")
+
+    expect(@user2.reload.username).to eq("Leo Soubeste")
+    expect(@user2.reload.email).to eq("leosoubeste@other.com")
+  end
 end
